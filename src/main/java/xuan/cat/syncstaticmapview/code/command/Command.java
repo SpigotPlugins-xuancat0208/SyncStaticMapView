a65ed5239a9c7cd4c4c0444eadeed0654492ef81
xuancat0208 <xuancat0208@gmail.com>
2021-09-22 09:42:23 +0800

    更新

--- src/main/java/xuan/cat/syncstaticmapview/code/command/Command.java
+++ src/main/java/xuan/cat/syncstaticmapview/code/command/Command.java
@@ -161,7 +161,7 @@ public final class Command implements CommandExecutor {
                                                                         HttpURLConnection connection    = (HttpURLConnection) url.openConnection();
                                                                         connection.setRequestMethod("GET");
                                                                         connection.setDoInput(true);
-                                                                        connection.setDoOutput(true);
+                                                                        connection.setDoOutput(false);
                                                                         connection.setRequestProperty("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
                                                                         connection.setRequestProperty("accept-encoding", "gzip, deflate, br");
                                                                         connection.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36");
